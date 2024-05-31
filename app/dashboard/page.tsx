2882fce759978ad805573affe132d4517802a8e7
ho-shinjo-bf <ho.shinjo@basefood.co.jp>
2024-05-31 16:30:35 +0900

    chapter 6

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,15 @@
+import { useEffect, useState } from 'react';
+
 export default function Page() {
-  return <p>Dashboard Page</p>;
+  const [googleHtml, setGoogleHtml] = useState<string | null>(null);
+
+  useEffect(() => {
+    const getGoogleHtml = async () => {
+      const googleHtml = await fetch('https://www.google.com');
+      setGoogleHtml(await googleHtml.text());
+    };
+    getGoogleHtml();
+  }, []);
+
+  return <p>{googleHtml}</p>;
 }
